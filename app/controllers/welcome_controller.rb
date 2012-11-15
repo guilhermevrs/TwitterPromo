171c61c2dfd583a8a521ed8480a3c8849e6b6cb8
guilhermevrs <guilhermevrs@gmail.com>
2012-11-15 21:05:33 -0200

    Adds a getTweetsFromServer method to the PromoTweet model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,13 +2,6 @@ gem 'twitter'
 
 class WelcomeController < ApplicationController
 	def index
-	  	@arrayTweet = Array.new()
-	  	Twitter.search("from:promocoes OR from:@boaspromocoes", :count => 10).results.map do |status|
-	  	  @newPromoTweet = PromoTweet.new(:user => status.from_user, :content => status.text)
-	  	  if(!status.urls.nil?)
-	  	  	@newPromoTweet.url = status.urls[0].expanded_url
-	  	  end
-		  @arrayTweet.push @newPromoTweet
-		end
+	  	@arrayTweet = PromoTweet.getPromoTweetsFromServer("from:promocoes", 100)
 	end
 end
--- app/models/promo_tweet.rb
+++ app/models/promo_tweet.rb
@@ -12,6 +12,21 @@ class PromoTweet
 		end
 	end
 
+	def self.getPromoTweetsFromServer(query, count)
+		@arrayTweet = Array.new()
+	  	Twitter.search(query, :count => count).results.map do |status|
+	  	  @newPromoTweet = PromoTweet.new(:user => status.from_user, :content => status.text)
+	  	  if(!status.urls.nil?)
+	  	  	@newPromoTweet.url = status.urls[status.urls.length-1].expanded_url
+	  	  	status.urls.each do |ur|
+	  	  		@newPromoTweet.content = @newPromoTweet.content.gsub(ur.url, "")
+	  	  	end
+	  	  end
+		  @arrayTweet.push @newPromoTweet
+		end
+		return @arrayTweet
+	end
+
 	def persisted?
 		false
 	end
